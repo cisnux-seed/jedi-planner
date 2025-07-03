36d4ccb17f9c524d209281dbfa9d75655dcdda9f
cisnux <fajrarisqulla@gmail.com>
2025-07-03 14:07:54 +0700

    fix: error message

--- src/main/kotlin/org/cisnux/jediplanner/applications/controllers/FileController.kt
+++ src/main/kotlin/org/cisnux/jediplanner/applications/controllers/FileController.kt
@@ -1,5 +1,6 @@
 package org.cisnux.jediplanner.applications.controllers
 
+import org.cisnux.jediplanner.commons.exceptions.APIException
 import org.cisnux.jediplanner.domains.storages.FileManager
 import org.springframework.core.io.Resource
 import org.springframework.http.HttpHeaders
@@ -19,9 +20,16 @@ class FileController(private val fileManager: FileManager) {
     @GetMapping("/{filename:.+}", produces = [MediaType.APPLICATION_OCTET_STREAM_VALUE])
     @ResponseStatus(HttpStatus.OK)
     suspend fun getFile(@PathVariable filename: String): ResponseEntity<Resource> {
-        val resource = fileManager.getResource(filename)
-        return ResponseEntity.ok()
-            .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + resource.filename + "\"")
-            .body<Resource>(resource)
+        try {
+            val resource = fileManager.getResource(filename)
+            return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + resource.filename + "\"")
+                .body(resource)
+        } catch (e: NoSuchFileException){
+            throw APIException.NotFoundResourceException(
+                statusCode = HttpStatus.NOT_FOUND.value(),
+                message = "file not found: $filename"
+            )
+        }
     }
 }
